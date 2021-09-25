d9cf9ae2e8ee3a40f576fb8b84e53dc7d162595a
OniAkuma96 <harrison.tudball@hotmail.com>
2021-09-25 16:59:26 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JddoMFHhgdOK0dN17van7D3Vm9BvlznJgiwxWR9bHAtAR8M41wSC18hwOIDLuiHnd5aUZ2sIHiE4fAyfrzn6Eaw00roFWfBEn',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
